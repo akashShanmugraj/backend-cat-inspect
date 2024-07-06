bd1c98da1b44e01a93ab8fd4c43cfe518237ca3e
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-07-06 10:48:14 +0530

    removed gcs

--- controllers/cloudstoragecontroller.js
+++ controllers/cloudstoragecontroller.js
@@ -1,7 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const { Storage } = require("@google-cloud/storage");
 const bcrypt = require("bcrypt");
-require("dotenv").config();
 
 const hashingfunction = async (string) => {
     const salt = await bcrypt.genSalt(10);
@@ -17,38 +16,17 @@ function getFileExtension(filename) {
     return filename.split('.').pop();
 }
 
-const storage = new Storage({
-    projectId: process.env.GCLOUD_PROJECT_ID
-});
+// const storage = new Storage({
+//     projectId: process.env.GCLOUD_PROJECT_ID
+// });
 
-const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET_NAME)
+// const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET_NAME)
 
 const uploadFile = asyncHandler(async (req, res) => {
     if (!req.files || !req.files.UploadFileField) {
         return res.status(400).send('No file was uploaded.');
     }
-    const hashedfilename = await hashingfunction(req.files.UploadFileField.name);
-    const fileextension = getFileExtension(req.files.UploadFileField.name);
-    const blob = bucket.file(`${req.body.component}/${hashedfilename}.${fileextension}`);
-    const blobStream = blob.createWriteStream({
-        resumable: false,
-    });
-
-    blobStream.on("error", (err) => {
-        res.status(500).send(err.message); // It's better to send the error message back to the client
-    });
-
-    blobStream.on("finish", () => {
-        // return public url 
-        publicurl = `https://storage.googleapis.com/${process.env.GCLOUD_STORAGE_BUCKET_NAME}/${req.body.component}/${hashedfilename}.${fileextension}`;
-        res.status(200).send({
-            message: 'File uploaded successfully',
-            publicurl: publicurl
-        });
-    });
-
-    // Use the buffer from the uploaded file
-    blobStream.end(req.files.UploadFileField.data);
+    res.status(200).send('File uploaded!');
 });
 
 module.exports = { uploadFile };
